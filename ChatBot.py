7042d177d9736baf5db142b8b258f6d0e0c6192a
Ondrej Kral <ondrej_kral@hotmail.com>
2018-03-06 21:28:08 +0100

    Add Chatbot.

---
+++ ChatBot.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  5 21:35:51 2018
+
+@author: ondrejkral
+"""
+
+
+# Define variables
+name = "Greg"
+weather = "cloudy"
+
+# Define a dictionary with the predefined responses
+responses = {
+  "what's your name?": "my name is {0}".format(name),
+  "what's today's weather?": "the weather is {0}".format(weather),
+  "default": "default message"
+}
+
+# Return the matching response if there is one, default otherwise
+def respond(message):
+    # Check if the message is in the responses
+    if message in responses:
+        # Return the matching message
+        bot_message = responses[message]
+    else:
+        # Return the "default" message
+        bot_message = responses["default"]
+    return bot_message
+
+
+send_message("what's your favorite color?")
